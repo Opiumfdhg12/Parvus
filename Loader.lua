9148462f11fa54eecfb35a072e4b518095b25dbb
Opiumfdhg12 <tux-washer-dowry@duck.com>
2024-10-30 18:49:12 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,37 +1,33 @@
 repeat task.wait() until game.IsLoaded
 repeat task.wait() until game.GameId ~= 0
 
-if Parvus and Parvus.Loaded then
+if Parvus and Parvus.Utilities and Parvus.Utilities.UI then
     Parvus.Utilities.UI:Push({
         Title = "Parvus Hub",
         Description = "Script already running!",
         Duration = 5
-    }) return
+    })
+    return
 end
-
---[[if Parvus and (Parvus.Game and not Parvus.Loaded) then
-    Parvus.Utilities.UI:Push({
-        Title = "Parvus Hub",
-        Description = "Something went wrong!",
-        Duration = 5
-    }) return
-end]]
 
 local PlayerService = game:GetService("Players")
 repeat task.wait() until PlayerService.LocalPlayer
 local LocalPlayer = PlayerService.LocalPlayer
 
 local Branch, NotificationTime, IsLocal = ...
---local ClearTeleportQueue = clear_teleport_queue
 local QueueOnTeleport = queue_on_teleport
 
 local function GetFile(File)
-    return IsLocal and readfile("Parvus/" .. File)
-    or game:HttpGet(("%s%s"):format(Parvus.Source, File))
+    return IsLocal and readfile("Parvus/" .. File) or game:HttpGet(("%s%s"):format(Parvus.Source, File))
 end
 
 local function LoadScript(Script)
-    return loadstring(GetFile(Script .. ".lua"), Script)()
+    local content = GetFile(Script .. ".lua")
+    if content then
+        return loadstring(content, Script)()
+    else
+        warn("Failed to load script: " .. Script)
+    end
 end
 
 local function GetGameInfo()
@@ -40,23 +36,21 @@ local function GetGameInfo()
             return Info
         end
     end
-
     return Parvus.Games.Universal
 end
 
 getgenv().Parvus = {
     Source = "https://raw.githubusercontent.com/AlexR32/Parvus/" .. Branch .. "/",
-
     Games = {
-        ["Universal" ] = { Name = "Universal",                  Script = "Universal"  },
-        ["1168263273"] = { Name = "Bad Business",               Script = "Games/BB"   },
-        ["3360073263"] = { Name = "Bad Business PTR",           Script = "Games/BB"   },
-        ["1586272220"] = { Name = "Steel Titans",               Script = "Games/ST"   },
-        ["807930589" ] = { Name = "The Wild West",              Script = "Games/TWW"  },
-        ["580765040" ] = { Name = "RAGDOLL UNIVERSE",           Script = "Games/RU"   },
-        ["187796008" ] = { Name = "Those Who Remain",           Script = "Games/TWR"  },
-        ["358276974" ] = { Name = "Apocalypse Rising 2",        Script = "Games/AR2"  },
-        ["3495983524"] = { Name = "Apocalypse Rising 2 Dev.",   Script = "Games/AR2"  },
+        ["Universal"] = { Name = "Universal", Script = "Universal" },
+        ["1168263273"] = { Name = "Bad Business", Script = "Games/BB" },
+        ["3360073263"] = { Name = "Bad Business PTR", Script = "Games/BB" },
+        ["1586272220"] = { Name = "Steel Titans", Script = "Games/ST" },
+        ["807930589"] = { Name = "The Wild West", Script = "Games/TWW" },
+        ["580765040"] = { Name = "RAGDOLL UNIVERSE", Script = "Games/RU" },
+        ["187796008"] = { Name = "Those Who Remain", Script = "Games/TWR" },
+        ["358276974"] = { Name = "Apocalypse Rising 2", Script = "Games/AR2" },
+        ["3495983524"] = { Name = "Apocalypse Rising 2 Dev.", Script = "Games/AR2" },
         ["1054526971"] = { Name = "Blackhawk Rescue Mission 5", Script = "Games/BRM5" }
     }
 }
@@ -68,13 +62,10 @@ Parvus.Utilities.Drawing = LoadScript("Utilities/Drawing")
 
 Parvus.Cursor = GetFile("Utilities/ArrowCursor.png")
 Parvus.Loadstring = GetFile("Utilities/Loadstring")
-Parvus.Loadstring = Parvus.Loadstring:format(
-    Parvus.Source, Branch, NotificationTime, tostring(IsLocal)
-)
+Parvus.Loadstring = Parvus.Loadstring:format(Parvus.Source, Branch, NotificationTime, tostring(IsLocal))
 
 LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.InProgress then
-        --ClearTeleportQueue()
+    if State == Enum.TeleportState.InProgress and QueueOnTeleport then
         QueueOnTeleport(Parvus.Loadstring)
     end
 end)
